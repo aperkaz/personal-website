1a42b5705be82142e20afc49b89b93ddd41a43ef
alain <alainperkaz@gmail.com>
2021-11-27 16:27:42 +0100

    Add linkeding link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,10 +3,9 @@ import { PropsWithChildren } from 'react';
 import UnderlinedText from './underlined-text';
 
 interface Props extends BoxProps {
-  underlineColor: string;
+  underlineColor?: string;
   emoji?: string;
 }
-// TODONOW: add gradiend underlines
 const Header = ({ children, underlineColor, emoji, ...props }: PropsWithChildren<Props>) => (
   <Box as="h1" mt={10} mb={6} fontSize="3xl" lineHeight="shorter" fontWeight="bold" {...props}>
     <UnderlinedText color={underlineColor}>{children}</UnderlinedText>
